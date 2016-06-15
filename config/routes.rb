c843793edc4c2cf5043785fd57602ea271906c95
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-06-15 12:30:46 +0300

    Add comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :posts
+  
+  resources :posts do
+    resources :comments
+  end
 
   root to: 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
